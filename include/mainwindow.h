7f9be34c2a60565b9a0073a5666e855e8ab0638b
Park Byeong Gyu <42878943+GlennPark@users.noreply.github.com>
2023-01-09 16:15:45 +0900

    0109_01
    project backup

---
+++ include/mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+
+#include <QMainWindow.h>
+#include <QObject.h>
+
+//class vtkObject;
+//class vtkObjReader;
+//class vtkActor;
+//class vtkRenderer;
+//class vtkRendererWindow;
+//class vtkPolyDataMapper;
+//class vtkRenderWindowInterActor;
+//class vtkSmartPointer;
+//class QVTKOpenGLStereoWidget;
+
+//#include <QVTKWidget.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+   
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
