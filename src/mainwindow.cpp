730dfb815d3afd8dc02f5449c1c0659ac2aea29f
GlennPark <downgear@naver.com>
2023-01-11 15:13:28 +0900

    0111_01
    projectbackup

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,7 +10,7 @@
 #include <vtkNamedColors.h>
 #include <vtkProperty.h>
 #include <vtkPolyDataMapper.h>
-#include <vtkRenderWindowInterActor.h>
+#include <vtkRenderWindowInteractor.h>
 #include <vtkSmartPointer.h>
 #include <vtkGenericOpenGLRenderWindow.h>
 #include <QVTKOpenGLStereoWidget.h>
@@ -19,7 +19,7 @@
 #include <vtkCamera.h>
 #include <vtkNew.h>
 #include <vtkNamedColors.h>
-#include <QOpenGLWidget.h>
+#include <qopenglwidget.h>
 
 #include <vtkOpenGLRenderWindow.h>
 #include <vtkRenderWindow.h>
@@ -32,7 +32,6 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    
 
     vtkSmartPointer<vtkOBJReader> readerAll = vtkSmartPointer<vtkOBJReader>::New();
     readerAll->SetFileName("IronMan.obj");
@@ -43,7 +42,6 @@ MainWindow::MainWindow(QWidget *parent)
     vtkSmartPointer<vtkOBJReader> readerSub = vtkSmartPointer<vtkOBJReader>::New();
     readerSub->SetFileName("IronMan.obj");
     readerSub->Update();
-
 
     vtkSmartPointer<vtkNamedColors> colorsAll = vtkSmartPointer<vtkNamedColors>::New();
     vtkColor3d backgroundColorAll = colorsAll->GetColor3d("SpringGreen");
@@ -63,7 +61,6 @@ MainWindow::MainWindow(QWidget *parent)
     mapperMain->SetInputConnection(readerMain->GetOutputPort());
     vtkSmartPointer<vtkPolyDataMapper> mapperSub = vtkSmartPointer<vtkPolyDataMapper>::New();
     mapperSub->SetInputConnection(readerSub->GetOutputPort());
-    
 
     vtkSmartPointer<vtkActor> actorAll = vtkSmartPointer<vtkActor>::New();
     actorAll->SetMapper(mapperAll);
@@ -107,7 +104,7 @@ MainWindow::MainWindow(QWidget *parent)
     rendererSub->GetActiveCamera()->Azimuth(10);
     rendererSub->GetActiveCamera()->Elevation(10);
     rendererSub->ResetCameraClippingRange();
-    
+
     vtkSmartPointer<vtkGenericOpenGLRenderWindow> renderWindowAll = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
     renderWindowAll->AddRenderer(rendererAll);
     renderWindowAll->SetWindowName("readAll");
@@ -117,51 +114,62 @@ MainWindow::MainWindow(QWidget *parent)
     vtkSmartPointer<vtkGenericOpenGLRenderWindow> renderWindowSub = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
     renderWindowSub->AddRenderer(rendererSub);
     renderWindowSub->SetWindowName("readSub");
-    
-    ui->openGLWidget_All->setRenderWindow(renderWindowAll);
-    ui->openGLWidget_Main->setRenderWindow(renderWindowMain);
-    ui->openGLWidget_Sub->setRenderWindow(renderWindowSub);
-    
-    ui->openGLWidget_All->interactor()->SetRenderWindow(renderWindowAll);
-    ui->openGLWidget_Main->interactor()->SetRenderWindow(renderWindowMain);
-    ui->openGLWidget_Sub->interactor()->SetRenderWindow(renderWindowSub);
-    
-    ui->openGLWidget_All->interactor()->ProcessEvents();
-    ui->openGLWidget_Main->interactor()->ProcessEvents();
-    ui->openGLWidget_Sub->interactor()->ProcessEvents();
-    
+
     rendererAll->AddActor(actorAll);
     rendererMain->AddActor(actorMain);
     rendererSub->AddActor(actorSub);
-    
+
+    ui->openGLWidget_All->setRenderWindow(renderWindowAll);
+    ui->openGLWidget_Main->setRenderWindow(renderWindowMain);
+    ui->openGLWidget_Sub->setRenderWindow(renderWindowSub);
+
+    ui->openGLWidget_All->interactor()->SetRenderWindow(renderWindowAll);
+    ui->openGLWidget_Main->interactor()->SetRenderWindow(renderWindowMain);
+    ui->openGLWidget_Sub->interactor()->SetRenderWindow(renderWindowSub);
+
+    ui->openGLWidget_All->interactor()->ProcessEvents();
+    ui->openGLWidget_Main->interactor()->ProcessEvents();
+    ui->openGLWidget_Sub->interactor()->ProcessEvents();
+
     ui->openGLWidget_All->interactor()->Start();
     ui->openGLWidget_Main->interactor()->Start();
     ui->openGLWidget_Sub->interactor()->Start();
-    
-    vtkSmartPointer<vtkTransform> transformAll = vtkSmartPointer<vtkTransform>::New();
-    transformAll->Translate(5 / 50, 0, 100);
-    actorAll->SetUserTransform(transformAll);
 
-    vtkSmartPointer<vtkTransform> transformMain = vtkSmartPointer<vtkTransform>::New();
-    transformMain->RotateX(180);
-    actorMain->SetUserTransform(transformMain);
+//    vtkSmartPointer<vtkTransform> transformAll = vtkSmartPointer<vtkTransform>::New();
+//    transformAll->Translate(5 / 50, 0, 100);
+//    actorAll->SetUserTransform(transformAll);
 
-    vtkSmartPointer<vtkTransform> transformSub = vtkSmartPointer<vtkTransform>::New();
-    transformSub->Translate(5 / 50, 100, 0);
-    actorSub->SetUserTransform(transformSub);
+//    vtkSmartPointer<vtkTransform> transformMain = vtkSmartPointer<vtkTransform>::New();
+//    transformMain->RotateX(180);
+//    actorMain->SetUserTransform(transformMain);
 
-    renderWindowAll->Render();
-    renderWindowMain->Render();
-    renderWindowSub->Render();
+//    vtkSmartPointer<vtkTransform> transformSub = vtkSmartPointer<vtkTransform>::New();
+//    transformSub->Translate(5 / 50, 100, 0);
+//    actorSub->SetUserTransform(transformSub);
 
-}   
+//    renderWindowAll->Render();
+//    renderWindowMain->Render();
+//    renderWindowSub->Render();
+
+    connect(ui->MainPushButton, SIGNAL(clicked), this, SLOT(MainPushButton_Clicked));
+    connect(ui->SubPushButton, SIGNAL(clicked), this, SLOT(SubPushButton_Clicked));
+
+
+}
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+void rotateMain()
+{
+
+}
+
+
 void loadObj()
 {
-   
+
 }
+
